20e33693e606df7c73646269c3eb3dc07ec35f08
Elyance <rahantamalala23@gmail.com>
2025-03-24 18:55:46 +0300

    Add accueil route and view for employee dashboard; refactor login method

--- app/config/routes.php
+++ app/config/routes.php
@@ -16,4 +16,6 @@ $router->group('/employe', function() use ($router, $app) {
 	$Employe_Controller = new EmployeController();
 	$router->get('/', [ $Employe_Controller, 'getFormulaireLogin']);
 	$router->post('/doLogin', [ $Employe_Controller, 'login' ]);
+	$router->get('/accueil', [ $Employe_Controller, 'getAccueil']);
+
 });
